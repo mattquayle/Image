f389149eb3b7bf2544b2e576e99a3eabe90d77a9
mattquayle <quayle.matt@gmail.com>
2023-02-04 19:53:58 +0000

    First commit

---
+++ program.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using System.Net.Mime;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace ResizeImageCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var image = 
+            //original dimension of image 223 x 240
+            Bitmap img = new Bitmap(@"E:\sampleimage\image.png"); 
+            //we are resizing to 110 x 120
+            var resizedImg= ResizeImage(img, 110, 120);
+            //save file
+            resizedImg.Save(@"E:\sampleimage\resizedImage.png");
+        }
+        //image resize method
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            //create new destImage object
+            var destImage = new Bitmap(width, height);
+
+            //maintains DPI regardless of physical size
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                //determines whether pixels from a source image overwrite or are combined with background pixels.
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                //determines the rendering quality level of layered images.
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                // determines how intermediate values between two endpoints are calculated
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                //specifies whether lines, curves, and the edges of filled areas use smoothing 
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                //affects rendering quality when drawing the new image
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    //prevents ghosting around the image borders
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
