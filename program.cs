178befa6187763d54f538bd09dd50b4a393ac702
Matthew Quayle <quayle.matt@gmail.com>
2023-02-04 23:50:46 +0000

    convert png to jpg and resize dpi, width and height

--- program.cs
+++ program.cs
@@ -1,7 +1,10 @@
 ﻿using System.Reflection;
 using System.Net.Mime;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Metadata;
 using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Transforms;
+using SixLabors.ImageSharp.Formats.Jpeg;
 
 namespace ResizeImageCsharp
 {
@@ -9,46 +12,26 @@ namespace ResizeImageCsharp
     {
         static void Main(string[] args)
         {
-            using var image = 
-            //original dimension of image 223 x 240
-            Bitmap img = new Bitmap(@"E:\sampleimage\image.png"); 
-            //we are resizing to 110 x 120
-            var resizedImg= ResizeImage(img, 110, 120);
-            //save file
-            resizedImg.Save(@"E:\sampleimage\resizedImage.png");
-        }
-        //image resize method
-        public static Bitmap ResizeImage(Image image, int width, int height)
-        {
-            var destRect = new Rectangle(0, 0, width, height);
-            //create new destImage object
-            var destImage = new Bitmap(width, height);
-
-            //maintains DPI regardless of physical size
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage))
+            using var imagePNG = Image.Load("Image.png");
             {
-                //determines whether pixels from a source image overwrite or are combined with background pixels.
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                //determines the rendering quality level of layered images.
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                // determines how intermediate values between two endpoints are calculated
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                //specifies whether lines, curves, and the edges of filled areas use smoothing 
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                //affects rendering quality when drawing the new image
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                using (var wrapMode = new ImageAttributes())
-                {
-                    //prevents ghosting around the image borders
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
+                imagePNG.SaveAsJpeg("image.jpg");
             }
+            using var image = Image.Load("image.jpg");
+            {
+                ImageMetadata metadata = image.Metadata;
+                double currentDPI = metadata.HorizontalResolution;
+                double resizeRatio = 300 / currentDPI;
 
-            return destImage;
+			    int targetWidth = (int)Math.Round((image.Width * resizeRatio)); 
+			    int targetHeight = (int)Math.Round((image.Height * resizeRatio));
+                
+			    metadata.HorizontalResolution = 300;
+                metadata.VerticalResolution = 300;
+
+                image.Mutate(x => x.Resize(targetWidth, targetHeight));
+
+                image.Save("imageResized.jpg");
+            }
         }
     }
 }
